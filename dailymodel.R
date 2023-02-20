b32728d810743a910278d9b1f8fa71cab466dd42
David Jamissen <dja@live.no>
2023-02-20 12:03:42 +0100

    changing to hdd16

--- dailymodel.R
+++ dailymodel.R
@@ -6,8 +6,12 @@ library(car)
 
 # Load heating degree days / weather  data
 weather <- read_csv("./data/IEAhdd18_deu.csv", col_names = TRUE, cols("Date" = col_date(format = "%Y-%m-%d")))[c(4,5)]
+weather_th <- read_csv("./data/hddth18.csv", col_names = TRUE, cols("Date" = col_date(format = "%Y-%m-%d")))
+weather_16 <- read_csv("./data/hdd16.csv", col_names = TRUE, cols("Date" = col_date(format = "%Y-%m-%d")))
 # Adding infinitesimal to heating degree days to prevent zero values
-weather$HDD18 <- weather$HDD18 + 0.00000001
+# weather$HDD18 <- weather$HDD18 + 0.00000000000001
+# weather_th$HDDThold18 <- weather_th$HDDThold18 + 0.00000000000001
+# weather$HDD16 <- weather$HDD16 + 0.00000000000001
 
 # Load natural gas consumption data
 demand <- read_csv("./data/gastag.csv", 
@@ -24,9 +28,11 @@ eex <- read_csv("data/eex.csv",
 
 # Merging dataset
 data <- merge(weather, demand, by = 1)
-data <- merge(data, ttfclose, by = 1)
+# data <- merge(data, ttfclose, by = 1)
 data <- merge(data, eex[c(1, 3)], by = 1)
-colnames(data) <- c("date", "hdd", "demand", "ttf", "eex")
+data <- merge(data, weather_th, by = 1)
+data <- merge(data, weather_16, by = 1)
+colnames(data) <- c("date", "hdd", "demand", "eex", "hddth", "hdd16")
 
 # Weekly dataset
 library(ISOweek)
@@ -42,17 +48,19 @@ data$week <- substr(as.character(data$yearweek), 7, 8)
 # Group by iso-year and year, and calculate the sum of hdd, demand, and the mean of ttf
 data_weekly <- group_by(data, yearweek) %>% summarize(hdd = sum(hdd),
                                                       demand = sum(demand),
-                                                      ttf = mean(ttf))
+                                                     # ttf = mean(ttf)
+                                                     )
 
 data_monthly <- group_by(data, yearweek) %>% summarize(hdd = sum(hdd),
                                                        demand = sum(demand),
-                                                       ttf = mean(ttf))
+                                                       #ttf = mean(ttf)
+                                                       )
 # Demeaned, seasonal adjusted
 demdata <- data %>% 
   group_by(week) %>% 
-  mutate(demand = demand - mean(demand), hdd = hdd - mean(hdd))
+  mutate(demand = demand - mean(demand), hdd = hdd - mean(hdd), hddth = hddth - mean(hddth), hdd16 = hdd16 - mean(hdd16))
 
-demdata$ttf_logret <- log(demdata$ttf) - log(dplyr::lag(demdata$ttf))
+# demdata$ttf_logret <- log(demdata$ttf) - log(dplyr::lag(demdata$ttf))
 demdata$eex_lag <- dplyr::lag(demdata$eex)
 
 demdata <- drop_na(demdata)
@@ -60,23 +68,18 @@ demdata <- drop_na(demdata)
 
 
 # ARDL model demand
-model <- auto_ardl(demand ~ hdd + eex_lag, data = demdata, max_order = 5, selection = "BIC")$best_model
+model <- auto_ardl(demand ~ hdd16 + eex, data = demdata, max_order = 5, selection = "AIC")$best_model
 summary(model)
 multipliers(model)
 multipliers(model, type = "sr")
 
 # Simple linear regression
-lm <- lm(demand ~ hdd + dplyr::lag(hdd) + eex, data = demdata)
+lm <- lm(demand ~ hdd16 + dplyr::lag(hdd16) + eex, data = demdata)
 # Checking linear relationship
-crPlot(lm, variable = "eex")
+crPlot(lm, variable = "hdd16")
 summary(lm)
 
-predicted_values <- predict(lm)
 
-# Calculate the residuals
-residuals <- demdata$demand - c(NA, predicted_values)
-
-plot(demdata$date, residuals)
 
 # Elasticity estimates
 avg_price <- mean(demdata$eex_lag)
