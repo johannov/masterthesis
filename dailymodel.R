8ef656d04cfcba22435475834ca72f3dc4dc116a
David Jamissen <dja@live.no>
2023-02-22 10:44:03 +0100

    more weather variables

--- dailymodel.R
+++ dailymodel.R
@@ -8,6 +8,12 @@ library(car)
 weather <- read_csv("./data/IEAhdd18_deu.csv", col_names = TRUE, cols("Date" = col_date(format = "%Y-%m-%d")))[c(4,5)]
 weather_th <- read_csv("./data/hddth18.csv", col_names = TRUE, cols("Date" = col_date(format = "%Y-%m-%d")))
 weather_16 <- read_csv("./data/hdd16.csv", col_names = TRUE, cols("Date" = col_date(format = "%Y-%m-%d")))
+weather_wind <- read_csv("./data/hddwind.csv", col_names = TRUE, cols("Date" = col_date(format = "%Y-%m-%d")))
+weather_wind10 <- read_csv("./data/wind.csv", col_names = TRUE, cols("Date" = col_date(format = "%Y-%m-%d")))
+weather_evap <- read_csv("./data/evap.csv", col_names = TRUE, cols("Date" = col_date(format = "%Y-%m-%d")))
+weather_humid <- read_csv("./data/humid.csv", col_names = TRUE, cols("Date" = col_date(format = "%Y-%m-%d")))
+weather_cloud <- read_csv("./data/cloud.csv", col_names = TRUE, cols("Date" = col_date(format = "%Y-%m-%d")))
+
 # Adding infinitesimal to heating degree days to prevent zero values
 # weather$HDD18 <- weather$HDD18 + 0.00000000000001
 # weather_th$HDDThold18 <- weather_th$HDDThold18 + 0.00000000000001
@@ -32,7 +38,15 @@ data <- merge(weather, demand, by = 1)
 data <- merge(data, eex[c(1, 3)], by = 1)
 data <- merge(data, weather_th, by = 1)
 data <- merge(data, weather_16, by = 1)
-colnames(data) <- c("date", "hdd", "demand", "eex", "hddth", "hdd16")
+data <- merge(data, weather_wind, by = 1)
+data <- merge(data, weather_wind10, by = 1)
+data <- merge(data, weather_evap, by = 1)
+data <- merge(data, weather_humid, by = 1)
+data <- merge(data, weather_cloud, by = 1)
+colnames(data) <- c("date", "hdd", "demand", "eex", "hddth", "hdd16", "hddwind", "wind", "evap", "humid", "cloud")
+
+# Convert demand to MWh
+data$demand <- data$demand * 0.001
 
 # Weekly dataset
 library(ISOweek)
@@ -58,40 +72,65 @@ data_monthly <- group_by(data, yearweek) %>% summarize(hdd = sum(hdd),
 # Demeaned, seasonal adjusted
 demdata <- data %>% 
   group_by(week) %>% 
-  mutate(demand = demand - mean(demand), hdd = hdd - mean(hdd), hddth = hddth - mean(hddth), hdd16 = hdd16 - mean(hdd16))
+  mutate(demand = demand - mean(demand), hdd = hdd - mean(hdd), hddth = hddth - mean(hddth), hdd16 = hdd16 - mean(hdd16), hddwind = hddwind - mean(hddwind), wind = wind - mean(wind), evap = evap - mean(evap), humid = humid - mean(humid), cloud = cloud - mean(cloud))
 
 # demdata$ttf_logret <- log(demdata$ttf) - log(dplyr::lag(demdata$ttf))
 demdata$eex_lag <- dplyr::lag(demdata$eex)
-
+demdata$eex_lagsq <- demdata$eex_lag * demdata$eex_lag
+demdata$eex_laglog <- log(demdata$eex_lag)
+demdata$eex_cap <- ifelse(demdata$eex > 60, 60, demdata$eex)
+demdata$eex_peak <- ifelse(demdata$eex > 60, demdata$eex - 60, 0)
+demdata$eex_cap_lag <- ifelse(demdata$eex_lag > 60, 60, demdata$eex_lag)
+demdata$eex_peak_lag <- ifelse(demdata$eex_lag > 60, demdata$eex_lag - 60, 0)
+demdata$eex_prewar <- ifelse(demdata$date < as.Date("2022-02-24"), demdata$eex_lag, 0)
+demdata$eex_postwar <- ifelse(demdata$date >= as.Date("2022-02-24"), demdata$eex_lag, 0)
 demdata <- drop_na(demdata)
 
 
+## Month dummies ##
+
+# Add a new column for month
+demdata <- demdata %>%
+  mutate(monthoy = format(date, "%m"))
+
+# Create month dummies using model.matrix()
+month_dummies <- model.matrix(~factor(monthoy)-1, data = demdata)
+
+# Add the dummies to the dataset
+demdata <- cbind(demdata, month_dummies)
+
+# Summer dummy
+demdata$summer <- demdata$`factor(monthoy)06` + demdata$`factor(monthoy)07` + demdata$`factor(monthoy)08`
+demdata$winter <- demdata$`factor(monthoy)12` + demdata$`factor(monthoy)01` + demdata$`factor(monthoy)02`
+
+demdata$humsum <- demdata$summer * demdata$humid
+demdata$humwin <- demdata$winter * demdata$humid
+
+demdata$hddwind <- demdata$hdd16 * demdata$hddwind
+
 
 # ARDL model demand
-model <- auto_ardl(demand ~ hdd16 + eex, data = demdata, max_order = 5, selection = "AIC")$best_model
+model <- auto_ardl(demand ~ cloud + wind + hdd16 + eex_lag, data = demdata, max_order = 5, selection = "BIC")$best_model
 summary(model)
 multipliers(model)
 multipliers(model, type = "sr")
 
 # Simple linear regression
-lm <- lm(demand ~ hdd16 + dplyr::lag(hdd16) + eex, data = demdata)
+lm <- lm(demand ~ eex_postwar + wind + hdd16 + cloud + dplyr::lag(hdd16), data = demdata)
 # Checking linear relationship
-crPlot(lm, variable = "hdd16")
+crPlot(lm, variable = "eex_postwar")
 summary(lm)
 
-
-
 # Elasticity estimates
-avg_price <- mean(demdata$eex_lag)
-avg_demand <- mean(data$demand) # data or dem dama?
+avg_price <- mean(subset(data, date > as.Date("2022-02-24"))$eex)
+avg_demand <- mean(subset(data, date > as.Date("2022-02-24"))$demand) # data or dem dama?
 
-elasticity <- multipliers(model)$estimate[3] * (avg_price / avg_demand)
-"Long run elasticity:" 
-elasticity
+# elasticity <- multipliers(model)$estimate[3] * (avg_price / avg_demand)
+# "Long run elasticity:" 
+# elasticity
 
 # Plot fit
-if (!exists("plot_demand")) {
-  source("plotdemand.R")
-}
+# if (!exists("plot_demand")) {
+#   source("plotdemand.R")
+# }
 
-plot_demand(model, demdata)
