f9b5c07d9d47682a8b50424fec35bccf3ebaf8f0
David Jamissen <dja@live.no>
2023-03-14 11:07:07 +0100

    weather

--- weather_analysis.R
+++ weather_analysis.R
@@ -3,6 +3,10 @@ library(tidyverse)
 library(ISOweek)
 library(tseries)
 
+# Load natural gas consumption data
+demand <- read_csv("./data/230222_Restlast.csv", col_types = cols(date = col_date(format = "%Y-%m-%d")))
+demand <- subset(demand, date != as.Date("2019-01-01"))
+
 # Weather, Population from states
 weather_statemean <- read_csv("data/dwd_pop_weighted_weather_mean.csv")
 
@@ -17,42 +21,56 @@ df <- pivot_wider(df, values_from = "value", names_from = "parameter", id_cols =
 df$celc <- df$temperature_air_mean_200 - 273.15
 df$hdd16 <- ifelse(df$celc < 16, 16 - df$celc, 0)
 
-total_pop <-  sum(df$SUM_pop_de)
+total_pop <-  sum(pop_df$SUM_pop_de)
 
 df$weight <- df$SUM_pop_de / total_pop
 
-df <- drop_na(df, any_of(c("wind_speed", "cloud_cover_total", "temperature_air_mean_200",
+df_all <- drop_na(df, any_of(c("wind_speed", "cloud_cover_total", "temperature_air_mean_200",
                          "celc", "hdd16")))
 
-data <- df %>%
-  mutate(across(c("wind_speed", "cloud_cover_total", "temperature_air_mean_200",
-                  "sunshine_duration", "celc", "hdd16"), ~ . * weight))
+df_hdd <- drop_na(df, any_of(c("hdd16")))
 
 
-data <- data %>% 
-  group_by(date) %>% 
-  summarize(
-    wind_speed = sum(wind_speed),
-    temperature_air_mean_200 = sum(temperature_air_mean_200),
-    hdd16 = sum(hdd16),
-    cloud_cover_total = sum(cloud_cover_total),
-    sunshine_duration = sum(sunshine_duration),
-    weight = sum(weight),
-    celc = sum(celc)
-  )
+summarizeWeather <- function(data) {
 
-data$yearweek <- ISOweek(data$date)
-data$week <- substr(as.character(data$yearweek), 7, 8)
+  data <- df_all %>%
+    mutate(across(c("wind_speed", "cloud_cover_total", "temperature_air_mean_200",
+                    "sunshine_duration", "celc", "hdd16"), ~ . * weight))
+  
+  
+  data <- data %>% 
+    group_by(date) %>% 
+    summarize(
+      wind_speed = sum(wind_speed),
+      temperature_air_mean_200 = sum(temperature_air_mean_200),
+      hdd16 = sum(hdd16),
+      cloud_cover_total = sum(cloud_cover_total),
+      sunshine_duration = sum(sunshine_duration),
+      weight = sum(weight),
+      celc = sum(celc)
+    )
+  
+  data[c("wind_speed", "cloud_cover_total", "temperature_air_mean_200",
+         "sunshine_duration", "celc", "hdd16")] <-
+    data[c("wind_speed", "cloud_cover_total", "temperature_air_mean_200",
+                                                          "sunshine_duration", "celc", "hdd16")] / data$weight
 
-data$date <- as.Date(data$date)
+  
+  data$yearweek <- ISOweek(data$date)
+  data$week <- substr(as.character(data$yearweek), 7, 8)
+  
+  data$date <- as.Date(data$date)
+  
 
-# Load natural gas consumption data
-demand <- read_csv("./data/230222_Restlast.csv", col_types = cols(date = col_date(format = "%Y-%m-%d")))
-demand <- subset(demand, date != as.Date("2019-01-01"))
+  data <- merge(data, demand, by = "date")
+  
+  return(data)
+}
 
-data <- merge(data, demand, by = "date")
+data_all <- summarizeWeather(df_all)
+data_hdd <- summarizeWeather(df_hdd)
 
-data_comp <- merge(data, weather_statemean, by = "date")
+data_comp <- merge(data_all, weather_statemean, by = "date")
 
 # Demeaned, seasonal adjusted
 demdata <- data_comp %>% 
@@ -71,5 +89,5 @@ train_demdata <- subset(demdata, date < as.Date("2021-08-01"))
 lm_state <- lm(demand ~ hdd16_sum + dplyr::lag(hdd16_sum) + wind_speed_sum + cloud_cover_total_sum, data = train_demdata)
 summary(lm_state)
 
-lm_pop <- lm(demand ~ hdd16 + dplyr::lag(hdd16) + wind_speed + cloud_cover_total, data = demdata)
+lm_pop <- lm(demand ~ hdd16 + dplyr::lag(hdd16) + wind_speed + cloud_cover_total, data = train_demdata)
 summary(lm_pop)
